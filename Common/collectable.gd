f786e1f80c7c558037a97e018565a0512e240760
Beatrice Hahn <beatricehahn7@gmail.com>
2025-03-25 19:41:40 -0700

    Add custom signal that indicates which item is clicked

--- Common/collectable.gd
+++ Common/collectable.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
-@export var item_name : String = ""
+@export var item_name : String = "UnnamedItem"
 
-var is_found = false
+var is_clicked = false
+
+signal is_found(item_name)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,19 +17,20 @@ func _process(delta: float) -> void:
 	
 
 func mark_as_found() -> void:
-	print("You discovered: " + item_name)
+	print("You clicked on: " + item_name)
 
 
-#func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	#if event.is_action_pressed("click"):
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event.is_action_released("click") and mouse_entered:
+		if !is_clicked:
+			is_clicked = true
+			# mark_as_found()
+			emit_signal("is_found", item_name)
+			
+#func _input(event):
+	## Check that mouse click was released while mouse was still in collision shape
+	#if event.is_action_released("click"):
 		#if !is_found:
 			#is_found = true
 			#mark_as_found()
-			
-func _input(event):
-	# Check that mouse click was released while mouse was still in collision shape
-	if event.is_action_released("click"):
-		if !is_found:
-			is_found = true
-			mark_as_found()
 		
