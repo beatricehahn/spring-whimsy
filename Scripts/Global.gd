9df9d99a1b7e7e3d1a1d696ea779d823daa68e5f
Beatrice Hahn <beatricehahn7@gmail.com>
2025-04-29 22:20:39 -0700

    Replace colorRect BG with tile texture for better performance

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -1,5 +1,6 @@
 extends Node
 
+var save_path = "user://data.save"
 
 var mouse_norm = load("res://Assets/Interface/textures_ui/Cursor1.png")
 var mouse_click = load("res://Assets/Interface/textures_ui/Cursor2.png")
@@ -11,3 +12,11 @@ var Data = {
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+
+func save_data() -> void:
+	var file = FileAccess.open(save_path, FileAccess.WRITE)
+	file.store_var(Data)
+	
+
+func load_data():
+	pass
